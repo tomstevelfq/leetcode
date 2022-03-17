bf62a6da6f3e9a4f89be355f67ba7559265a8c95
fqman <tomsetvelfq@gmail.com>
2022-03-17 10:00:17 +0800

    zicuan

--- zicuan.cpp
+++ zicuan.cpp
@@ -13,7 +13,7 @@ int longestValidParentheses(string s) {
     if(s[0]=='('){
         vec[0]=1;
     }
-    for(int i=0;i<s.size();i++){
+    for(int i=1;i<s.size();i++){
         if(s[i]=='('){
             vec[i]=vec[i-1]+1;
         }else{
@@ -26,8 +26,8 @@ int longestValidParentheses(string s) {
                         num[i]=2;
                     }
                 }else{
-                    if(i-1-num[i-1]>=0&&s[i-1-num[i-1]]==')'){
-                        num[i]=num[i-1]+num[i-1-num[i-1]]+2;
+                    if(i-2-num[i-1]>=0&&s[i-2-num[i-1]]==')'){
+                        num[i]=num[i-1]+num[i-2-num[i-1]]+2;
                     }else{
                         num[i]=num[i-1]+2;
                     }
